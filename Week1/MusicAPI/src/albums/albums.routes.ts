26d8f325306184b6deb10faa355b8b1aa61f3851
Arleth <62979596+arleaf@users.noreply.github.com>
2023-07-02 22:18:21 -0700

    Add files via upload

---
+++ Week1/MusicAPI/src/albums/albums.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import * as AlbumsController from './albums.controller';
+
+const router = Router();
+router
+    .route('/albums')
+    .get(AlbumsController.readAlbums);
+
+router
+    .route('/albums/:artist')
+    .get(AlbumsController.readAlbumsByArtist);
+
+router
+    .route('/albums/search/artist/:search')
+    .get(AlbumsController.readAlbumsByArtistSearch);
+
+router
+    .route('/albums/search/description/:search')
+    .get(AlbumsController.readAlbumsByDescriptionSearch);
+
+router
+    .route('/albums')
+    .post(AlbumsController.createAlbum);
+
+router
+    .route('/albums')
+    .post(AlbumsController.updateAlbum);
+
+router
+    .route('/albums/:albumId')
+    .delete(AlbumsController.deleteAlbum);
+//router
+//  .route('/albums/:readtracks')
+// .get(AlbumsController.readTracks());
+
+
+export default router;
