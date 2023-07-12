f8d0b43f66eb02abe5f5c3cad03f8df42fa53428
Arleth <62979596+arleaf@users.noreply.github.com>
2023-07-11 20:50:05 -0700

    Add files via upload

---
+++ week3/simpleapp/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InfoComponent } from './info/info.component';
+imports: [
+
+  BrowserModule,
+
+  ReactiveFormsModule,
+
+  AppRoutingModule
+
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShopComponent,
+    InfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
