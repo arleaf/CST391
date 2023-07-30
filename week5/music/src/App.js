90cb72641973a63e47b8d33f0ed2cf606dd103be
Arleth <62979596+arleaf@users.noreply.github.com>
2023-07-30 13:56:04 -0700

    Add files via upload

--- week5/music/src/App.js
+++ week5/music/src/App.js
@@ -1,33 +1,49 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react";
 import Card from './Card';
+import './App.css';
 
 
 const App = () => {
-    return (
-        <div>
+    const [albumList, setAlbumList] = useState([
+        {
+            artistId: 0,
+            artist: 'Arctic Monkeys',
+            title: 'AM',
+            description: "The band's fifth studio album, AM, was released on 9 September 2013.",
+            year: 2013,
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/%22AM%22_%28Arctic_Monkeys%29.jpg/220px-%22AM%22_%28Arctic_Monkeys%29.jpg"
+        },
+        {
+            artistId: 1,
+            artist: 'Arctic Monkeys',
+            title: 'Favourite Worst Nightmare',
+            description: "Favourite Worst Nightmare is the second studio album by English rock band Arctic Monkeys, released on 23 April 2007 by Domino Recording Company.",
+            year: 2007,
+            image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Favourite_Worst_Nightmare.jpg/220px-Favourite_Worst_Nightmare.jpg"
+        },
+        {
+            artistId: 2,
+            artist: 'Drake',
+            title: 'Certified Lover Boy',
+            description: "Certified Lover Boy is the sixth studio album by Canadian rapper and singer Drake, released on September 3, 2021, by OVO Sound and Republic Records.",
+            year: 2021,
+            image: "https://upload.wikimedia.org/wikipedia/en/thumb/7/79/Drake_-_Certified_Lover_Boy.png/220px-Drake_-_Certified_Lover_Boy.png"
+        },
+    ]);
 
-            <h1>Music I like</h1>
-            <Card
-                albumTitle="AM"
-                albumDescription="The band's fifth studio album, AM, was released on 9 September 2013.[145] The album was recorded in Rancho de la Luna in Joshua Tree, California, and features guest appearances from Josh Homme of Queens of the Stone Age, Elvis Costello's drummer Pete Thomas and Bill Ryder-Jones of the Coral."
-                imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/%22AM%22_%28Arctic_Monkeys%29.jpg/220px-%22AM%22_%28Arctic_Monkeys%29.jpg"
-                buttonText="OK"
-            />
-            <Card
-                albumTitle="Favourite Worst Nightmare"
-                albumDescription="Favourite Worst Nightmare is the second studio album by English rock band Arctic Monkeys, released on 23 April 2007 by Domino Recording Company."
-                imgURL="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Favourite_Worst_Nightmare.jpg/220px-Favourite_Worst_Nightmare.jpg"
-                buttonText="OK"
-            />
-            <Card
-                albumTitle="Certified Lover Boy"
-                albumDescription="Certified Lover Boy is the sixth studio album by Canadian rapper and singer Drake, released on September 3, 2021, by OVO Sound and Republic Records."
-                imgURL="https://upload.wikimedia.org/wikipedia/en/thumb/7/79/Drake_-_Certified_Lover_Boy.png/220px-Drake_-_Certified_Lover_Boy.png"
-                buttonText="OK"
-            />
-        </div>
-    )
-}
+    const renderedList = () => {
+        return albumList.map((album) => {
+            return (
+                <Card
+                    albumTitle={album.title}
+                    albumDescription={album.description}
+                    buttonText='OK'
+                    imgURL={album.image}
+                />
+            );
+        });
+    };
+    return <div className="container">{renderedList()}</div>;
+};
 
 export default App;
