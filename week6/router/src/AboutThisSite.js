351f2ea3455a9b0d2f7d4a139fa5b98ff1b8471a
Arleth <62979596+arleaf@users.noreply.github.com>
2023-08-06 23:39:35 -0700

    Add files via upload

---
+++ week6/router/src/AboutThisSite.js
@@ -0,0 +1,9 @@
+import React from "react";
+const AboutThisSite = () => {
+    return (<div>
+        <h1>About our Company</h1>
+        <p>We specialize in making great products and services</p>
+    </div>);
+}
+
+export default AboutThisSite
---
+++ week6/router/src/ContactUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactUs = () => {
+    return (<div>
+        <h2>Super Duper Company HQ</h2>
+        <p>123 Corporate Circle</p>
+        <p>Arizona, AZ</p>
+        <p>(123)456-7890</p>
+    </div>);
+}
+
+export default ContactUs;
